fcb721488c04c9f5ab38b71fdad059483fdce285
tahabikki <tahabikki00@gmail.com>
2026-05-17 20:55:33 +0100

    Fix path issue - remove outputFileTracingRoot

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
-import path from "node:path";
 
 const nextConfig: NextConfig = {
-  outputFileTracingRoot: path.join(process.cwd(), "../"),
   images: {
     remotePatterns: [
       {
