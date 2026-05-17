a1c3af9c81bbcae71e1874b8857859c9923380d6
tahabikki <tahabikki00@gmail.com>
2026-05-17 20:54:22 +0100

    Fix next.config.ts - remove eslint option

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import path from "node:path";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(process.cwd(), "../"),
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
